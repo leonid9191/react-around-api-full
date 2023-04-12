b60b716a373bad29867ab9357e284a5a180c8465
Leonid <leonid0991@gmail.com>
2023-04-12 16:07:03 +0300

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -24,10 +24,10 @@ const cardsRouter = require('./routes/cards');
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
 const app = express();
-app.use(requestLogger);
 app.use(limiter);
 app.use(cors());
 app.options('*', cors()); // enable requests for all routes
+app.use(requestLogger);
 app.use(helmet());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
